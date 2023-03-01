4c093ed8e7c402a1fbe6521a71dd34be8ceb6106
nikitasoni025 <nikitasonii025@gmail.com>
2023-03-01 21:14:08 +0530

    ho gya

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 
 import './App.scss';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Homepage from './Pages/Homepage/Homepage';
 import "./Assets/fonts/stylesheet.scss";
 
 function App() {
   return (
-    <div className="App">
-      <Homepage/>
-     
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 
