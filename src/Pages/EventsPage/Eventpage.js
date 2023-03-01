3b90f8d109abfb3bb7203afd0359dbfb2acf597c
Sakshi153 <sakshiv11034@gmail.com>
2023-03-01 16:42:47 +0530

    hjfytf

---
+++ src/Pages/EventsPage/Eventpage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Eventpage.scss';
+
+const Eventpage = () => {
+  return (
+    <div className='Eventpage'>
+        <div className="eventpage-wrap">
+            <div className="eventnav">
+                <ul className="event-navmenu">
+                    <li><a href='#'>Shaurya Robotics</a></li>
+                    <li><a href='#'>Kaushalya Sports</a></li>
+                    <li><a href='#'>Bouddhiki Brain Teaser</a></li>
+                    <li><a href='#'>Parakral Technical </a></li>
+                    <li><a href='#'>Natraja Cultural</a></li>
+                    <li><a href='#'>Kautilya Acedmics</a></li>
+                </ul>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Eventpage
