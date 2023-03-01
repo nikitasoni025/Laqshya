8eddb57ad5df3c9076a96d99437e2f3e94b1cf0c
nikitasoni025 <nikitasonii025@gmail.com>
2023-03-01 16:27:45 +0530

    cfctct

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import "./navbar.scss";
+import{ CgUserlane } from "react-icons/cg"
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="main-navbar">
-            <div className="left-navbar"></div>
-            <div className="right-navbar"></div>
+            <div className="left-navbar">
+              <div className="logo"><h1>LAQSHYA</h1></div>
+              <ul className="menu">
+                <li>Home</li>
+                <li>Events</li>
+                <li>Gallery</li>
+                <li>Contact</li>
+              </ul>
+
+              
+            </div>
+            <div className="right-navbar">
+              <a href="#">Register <CgUserlane/></a>
+
+            
+            </div>
         </div>
     </div>
   )
