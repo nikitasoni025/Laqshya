3f1f54e7eb2e419ddfba744a71160bbd1c19d6ca
nikitasoni025 <nikitasonii025@gmail.com>
2023-03-01 13:41:27 +0530

    first comiit

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="main-navbar">
+            <div className="left-navbar"></div>
+            <div className="right-navbar"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/Pages/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import "./homepage.scss";
+
+const Homepage = () => {
+  return (
+    <div className='homepage'>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Homepage
