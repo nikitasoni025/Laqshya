4e3b52db4f6a95627c12f0b4514bbf4104afaf1c
nikitasoni025 <nikitasonii025@gmail.com>
2023-03-04 11:26:24 +0530

    m

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -16,7 +16,7 @@ const Homepage = () => {
         <div className="section">
           <div className="wrap-section">
 
-            {/* header section */}
+            {/* HEADER */}
 
             <div className="header">
               <img className='headerfade-lft' src={smokeimg} alt="smokeimg" />
@@ -41,10 +41,12 @@ const Homepage = () => {
               <div className="details-wrap">
 
                 <div className="about-lakshya-details">
-                  <h1>LAQSHYA <span>2K23</span></h1>
-                  <h3>Techno Management Sports & Cultural Fest</h3>
-                  <div className="about-wrap">
-                    <div className="abt-wrap-left">
+
+                  <div className="upper-about">
+                    <h1>LAQSHYA <span>2K23</span></h1>
+                    <h3>Techno Management Sports & Cultural Fest</h3>
+                    <div className="about-wrap">
+
                       <p><span className='main-highlight'>Laqshya 2k23</span> is a highly anticipated annual event that took place in the <span className="main-highlight"> Chhattrapati Shivaji Institute of Technology</span>. This event is a combination of techno, management, sports, and cultural activities, which brought together students from all over the region to showcase their skills and talents.</p>
                       <p>The event began with a grand opening ceremony, which included a colorful procession of students dressed in traditional attire. This was followed by various technical competitions such as coding challenges, robotics contests, and hackathons, which tested the students' problem-solving skills and technical knowledge.</p>
                       <p>In addition to technical events,<span className="main-highlight"> Laqshya 2k23</span> also featured a range of management games and simulations that aimed to challenge the participants' leadership and decision-making abilities. These activities included quizzes, case studies, and business plan competitions, which were judged by industry experts and renowned academics.
@@ -52,22 +54,28 @@ const Homepage = () => {
                       <p>Sports enthusiasts had a chance to participate in various indoor and outdoor sports competitions, such as cricket, Futsal, Arm Wrestling ,Angry Birds. These events not only promoted physical fitness and healthy competition but also provided an opportunity for students to showcase their teamwork and sportsmanship skills.
                       </p>
                       <p>Apart from technical, management, and sports events, the cultural events were the highlights of the festival. There were music and dance performances, and various other cultural activities that showcased the diverse talents and cultural heritage of the region.</p>
-                    </div>
-                    <div className="abt-wrap-right">
+
 
                     </div>
 
                   </div>
+                  <div className="lower-about">
+                    
+                  </div>
                 </div>
 
+                <div className="collage">
 
+
+
+                </div>
 
               </div>
             </div>
 
             {/* SECTION 3  */}
 
-           
+
 
 
           </div>
