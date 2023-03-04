e57412dd4a02fb8776cc41dd96c957fe2b197350
nikitasoni025 <nikitasonii025@gmail.com>
2023-03-04 13:55:45 +0530

    c

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -27,12 +27,9 @@ const Homepage = () => {
             {/* SECTION 1 start here */}
 
             <div className="section-1">
-
               <div className="banner">
                 <h1>JAI SHIVAJI HALLA BOL</h1>
-
               </div>
-
             </div>
 
             {/* SECTION 2 */}
@@ -40,14 +37,12 @@ const Homepage = () => {
             <div className="section-2">
               {/* DETAILS WRAP */}
               <div className="details-wrap">
-
+                
                 <div className="about-lakshya-details">
-
                   <div className="upper-about">
                     <h1>LAQSHYA <span>2K23</span></h1>
                     <h3>Techno Management Sports & Cultural Fest</h3>
                     <div className="about-wrap">
-
                       <p><span className='main-highlight'>Laqshya 2k23</span> is a highly anticipated annual event that took place in the <span className="main-highlight"> Chhattrapati Shivaji Institute of Technology</span>. This event is a combination of techno, management, sports, and cultural activities, which brought together students from all over the region to showcase their skills and talents.</p>
                       <p>The event began with a grand opening ceremony, which included a colorful procession of students dressed in traditional attire. This was followed by various technical competitions such as coding challenges, robotics contests, and hackathons, which tested the students' problem-solving skills and technical knowledge.</p>
                       <p>In addition to technical events,<span className="main-highlight"> Laqshya 2k23</span> also featured a range of management games and simulations that aimed to challenge the participants' leadership and decision-making abilities. These activities included quizzes, case studies, and business plan competitions, which were judged by industry experts and renowned academics.
@@ -55,26 +50,33 @@ const Homepage = () => {
                       <p>Sports enthusiasts had a chance to participate in various indoor and outdoor sports competitions, such as cricket, Futsal, Arm Wrestling ,Angry Birds. These events not only promoted physical fitness and healthy competition but also provided an opportunity for students to showcase their teamwork and sportsmanship skills.
                       </p>
                       <p>Apart from technical, management, and sports events, the cultural events were the highlights of the festival. There were music and dance performances, and various other cultural activities that showcased the diverse talents and cultural heritage of the region.</p>
-
-
                     </div>
-
                   </div>
-                  <div className="lower-about">
-                    
-                  </div>
+                  <div className="lower-about"></div>
                 </div>
 
+                {/* CPLLAGE SECTION */}
+
                 <div className="collage">
-
-
-
+                  <div className="collage-wrap">
+                    <div className="row">
+                      <img src="https://ik.imagekit.io/egjzyz51e/img_5_1677915210110.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1677915371515" alt="" /></div>
+                    <div className="row">
+                      <img src="https://ik.imagekit.io/egjzyz51e/img_3_1677915162450.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1677915392553" alt="" /></div>
+                    <div className="row">
+                      <img src="https://ik.imagekit.io/egjzyz51e/img_2_1677915120207.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1677915356389" alt="" /></div>
+                    <div className="row">
+                      <img src="https://ik.imagekit.io/egjzyz51e/img_4_1677915173888.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1677915472669" alt="" /></div>
+                  </div>
                 </div>
 
               </div>
             </div>
 
             {/* SECTION 3  */}
+
+
+            
 
 
 
