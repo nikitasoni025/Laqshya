cfc3d8298fc624ca5bd62094d3ee5f602d4bba98
Sakshi153 <sakshiv11034@gmail.com>
2023-03-11 18:09:35 +0530

    huyj

--- src/Pages/ContactPage/ConatctPage.js
+++ src/Pages/ContactPage/ConatctPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaRegAddressCard } from 'react-icons/fa';
 import Navbar from '../../Components/Navbar/Navbar';
 import './contactPage.scss'
 
@@ -6,21 +7,31 @@ const ConatctPage = () => {
   return (
     <div className='contact-page'>
       <div className='contact-wrap'>
-        <Navbar/>
-        <div className='greet'>
-          <h1> We'd like to hear from you!</h1>
-        </div>
-        <div className='contact-detail'>
-          <div className='address'>
-            <h3>Address:Shivaji nagar Balod road Durg</h3>
-            <h3>chhattisgarh - 491001</h3>
-            <h3>Contact no : 9669978555 , 7999507973</h3>
-          </div>
-          <div className='social-media'>
+        <Navbar />
+        <div className='contact-banner'>
 
+          <div className='greet'>
+            <h1> We'd like to hear from you!</h1>
+          </div>
+          <div className='contact-detail'>
+            <div className='address'>
+              <div className='address-card'>
+                <FaRegAddressCard className='con-icon' />
+                <h3>Address:Shivaji nagar Balod road Durg</h3>
+                <h3>chhattisgarh - 491001</h3>
+                <h3>Contact no : 9669978555 , 7999507973</h3>
+              </div>
+            </div>
+            <div className='social-media'>
+              <div className="social-card">
+                <a href=""> Facebook</a>
+                <a href="">Instagram</a>
+                <a href="">youtube</a>
+              </div>
+            </div>
           </div>
         </div>
-        
+
       </div>
     </div>
   )
