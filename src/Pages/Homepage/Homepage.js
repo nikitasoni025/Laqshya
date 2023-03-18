643a7a13ce57713f5bbceb4dd48820904c7a2833
nikitasoni025 <nikitasonii025@gmail.com>
2023-03-18 15:39:48 +0530

    ba

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -21,7 +21,7 @@ const Homepage = (props) => {
       window.addEventListener('load', () => {
         timer = setTimeout(() => {
           setPreloading(false);
-          sessionStorage.setItem('visited','true');
+          sessionStorage.setItem('visited', 'true');
         }, 3000)
       })
     } else {
@@ -65,16 +65,16 @@ const Homepage = (props) => {
     <div className='homepage'>
 
       {/* Preloader */}
-      <div className={preloading ? 'preloading' :'preloading preloadingends'}>
-      <button onClick={()=>{setPreloading(false);sessionStorage.setItem('visited','true')}}>Dive In</button>
+      <div className={preloading ? 'preloading' : 'preloading preloadingends'}>
+        <button onClick={() => { setPreloading(false); sessionStorage.setItem('visited', 'true') }}>Dive In</button>
         <div className="prelogo">
-          <h1 className={preloading ? '' :'fade'}>LAQSHYA</h1>
+          <h1 className={preloading ? '' : 'fade'}>LAQSHYA</h1>
 
-          <p className={preloading ? '' :'fade'}>In Assocoation With</p>
+          <p className={preloading ? '' : 'fade'}>In Assocoation With</p>
 
-          <img className={preloading ? '' :'fade'} src="https://ik.imagekit.io/dexo68yudb/Tec36_Logo_white_300x.png?updatedAt=1679068816840" alt="" />
+          <img className={preloading ? '' : 'fade'} src="https://ik.imagekit.io/dexo68yudb/Tec36_Logo_white_300x.png?updatedAt=1679068816840" alt="" />
 
-          
+
         </div>
       </div>
 
@@ -113,7 +113,7 @@ const Homepage = (props) => {
 
                 <div className="about-lakshya-details">
                   <div className="upper-about">
-                    <h1>LAQSHYA <span>2K23</span></h1>
+                    <h1>LAQSHYA <br /> <span>2K23</span></h1>
                     <h3>Techno Management Sports & Cultural Fest</h3>
                     <div className="about-wrap">
                       <p><span className='main-highlight'>Laqshya 2k23</span> is a highly anticipated annual event that took place in the <span className="main-highlight"> Chhattrapati Shivaji Institute of Technology</span>. This event is a combination of techno, management, sports, and cultural activities, which brought together students from all over the region to showcase their skills and talents.</p>
